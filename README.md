eb33f1de7176805942f03f241f79dfa786bf59d1
Oxium Nox <noxorum@gmail.com>
2018-11-18 20:15:48 -0600

    Model added and README updated

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 This is my second proyect of the course, a Item Catalog
 
 ## Preparation
-...
+You need to get installed the next package for python:
+
+sqlalchemy
+flask
+requests
+oauth2client
+json
 
 ## Running the app
 ...
---
+++ model/database_setup.py
@@ -0,0 +1,63 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email
+        }
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer,primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    name = Column(String(256),nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'user_id' : self.user_id,
+            'name': self.name
+        }
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(Category)
+    name = Column(String(256),nullable=False)
+    description = Column(String(256))
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'category_id' : self.category_id,
+            'name': self.name,
+            'description': self.description
+        }
+
+engine = create_engine('sqlite:///restaurants.db')
+
+
+Base.metadata.create_all(engine)
